015ba5eb998b8d363793d3c93c042b13a9d8fdfc
akilayuva <akilapadma97@gmail.com>
2017-08-23 16:34:41 +0530

    Create primebwintervels.c

---
+++ primebwintervels.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+clrscr();
+printf("Enter the intervels");
+scanf("%d %d",&a,&b);
+while(a<b)
+{
+a++;
+for(i=2;i<=b/2;i++)
+{
+if(a%2!=0)
+{
+printf("%d",a);
+}}
+getch();
+}
